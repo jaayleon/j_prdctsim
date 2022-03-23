06a596c766f6c2ceff1eba7f3d93752d77e32367
jaayleaon <jleon.batulayan@gmail.com>
2022-03-23 08:41:47 -0700

    Fix leaf rounding error

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="j_prdctsim",
-    version="0.0.1",
+    version="0.0.2",
     author="J Leon Batulayan",
     author_email="jleon.batulayan@gmail.com",
     description="A small example package",
